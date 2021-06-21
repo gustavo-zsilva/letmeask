b3688f3540fd8be8a68a052b6a435e2d097415ba
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-06-21 17:36:48 -0300

    NLW Day #1

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button text="Botão 1" />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
